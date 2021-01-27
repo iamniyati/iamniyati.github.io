cee7d464a6d938670b8d6ff10c592aa3542cee8e
niyati.shah <niyati.shah@jet.computer>
2021-01-27 14:33:01 -0500

    edit shell file

--- shell_check.sh
+++ shell_check.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-echos "hello world"
+#!/bin/sh
+## Example: a typical script with several problems
+for f in $(ls *.m3u)
+do
+  grep -qi hq.*mp3 $f \
+    && echo -e 'Playlist $f contains a HQ file in mp3 format'
+done
+
+
